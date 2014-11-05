9ed8a7f2c62e074d4c2ef982acbb1574d0a6a637
radicalsauce <krhale@gmail.com>
2014-11-05 12:29:32 -0800

    Added pieces for dis systems communication + dependencies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express'); // express server
 var bodyParser = require('body-parser'); // use body-parser for getting body of incoming requests
 var mongoose = require('mongoose'); // use mongoose for user authentication routes
+var zerorpc = require("zerorpc"); // message protocol for communication between servers
 
 // request handlers
 var user = require('./users/user-controller.js'); // user authentication routes
