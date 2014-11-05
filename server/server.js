4571285ccc8ecf284ddbadd183e407f9f3f39d4c
Chao Xue <xuechao@alum.mit.edu>
2014-11-04 19:23:59 -0800

    Basic set-up for hello world

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//serves the static contents in the client folder
+app.use(express.static(__dirname + '/../client'));
+
+//for every incoming request, the following will parse the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//listens to incoming requests
+app.listen(process.env.PORT || '8080');
+
+//exporting the express server
+module.exports = app;
