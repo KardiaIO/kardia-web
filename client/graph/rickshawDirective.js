c6fd4a711aa2b38bfe3fcc9189356ca92d3705e3
mattphoto <github@mattkim.com>
2014-11-07 17:14:14 -0800

    add rickshaw poc graph

---
+++ client/graph/rickshawDirective.js
@@ -0,0 +1,30 @@
+angular.module('ekg')
+.directive('rickshawChart', function () {
+  return {
+    scope: {
+      data: '=',
+      renderer: '='
+    },
+    template: '<div></div>',
+    restrict: 'E',
+    link: function postLink(scope, element, attrs) {
+      scope.$watchCollection('[data, renderer]', function(newVal, oldVal){
+        if(!newVal[0]){
+          return;
+        }
+
+        element[0].innerHTML ='';
+
+        var graph = new Rickshaw.Graph({
+          element: element[0],
+          width: attrs.width,
+          height: attrs.height,
+          series: [{data: scope.data, color: attrs.color}],
+          renderer: scope.renderer
+        });
+
+        graph.render();
+      });
+    }
+  };
+});
