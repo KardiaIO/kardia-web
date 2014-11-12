955860fc9738465bd2dfe3eb6b9c410f4075a5c4
Nick Litwin <nicklitwin88@gmail.com>
2014-11-12 13:11:27 -0800

    Fix linting errors in timeShift.js

--- client/scripts/timeShift.js
+++ client/scripts/timeShift.js
@@ -1,33 +1,35 @@
 angular.module('ekg')
-.controller('timeCtrl', function($scope, timeFactory){
-  $scope.time = timeFactory;
+  .controller('timeCtrl', function($scope, timeFactory){
+    $scope.time = timeFactory;
 
-  $scope.time.display = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    $scope.time.display = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-  $scope.increment = function() {
-    $scope.time.value = moment($scope.time.value).add(1000, 'seconds'); 
-    $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
-  }
+    $scope.increment = function() {
+      $scope.time.value = moment($scope.time.value).add(1000, 'seconds'); 
+      $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    };
 
-  $scope.iincrement = function() {
-    $scope.time.value = moment($scope.time.value).add(10000, 'seconds'); 
-    $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
-  }
-  
-  $scope.decrement = function() {
-    $scope.time.value = moment($scope.time.value).subtract(1000, 'seconds'); 
-    $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
-  }
-  $scope.ddecrement = function() {
-    $scope.time.value = moment($scope.time.value).subtract(10000, 'seconds'); 
-    $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
-  }
+    $scope.iincrement = function() {
+      $scope.time.value = moment($scope.time.value).add(10000, 'seconds'); 
+      $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    };
+    
+    $scope.decrement = function() {
+      $scope.time.value = moment($scope.time.value).subtract(1000, 'seconds'); 
+      $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    };
 
-})
-.factory( 'timeFactory', function() {
-  var value = moment(new Date());
-  return {
-    value: value
-  }
-});
+    $scope.ddecrement = function() {
+      $scope.time.value = moment($scope.time.value).subtract(10000, 'seconds'); 
+      $scope.time.display = moment($scope.time.value).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    };
+
+  })
+
+  .factory('timeFactory', function() {
+    var value = moment(new Date());
+    return {
+      value: value
+    };
+  });
 
