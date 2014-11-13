bbdc31068610b1a715703a4ac91f9a590cb0d336
Nick Litwin <nicklitwin88@gmail.com>
2014-11-13 12:14:09 -0800

    Add a few tests and set up structure for server data tests

--- tests/serverSpecs/serverSpecs.js
+++ tests/serverSpecs/serverSpecs.js
@@ -1,57 +1,84 @@
 var expect = require('chai').expect;
 var request = require('request');
 var jwt = require('jwt-simple');
-// Require server.js so we have access to the mongoose connection
+// Require the user controller so we have access to the mongoose connection
 var dbConnection = require('../../server/users/user-controller');
 var User = require('../../server/users/user-model');
 
 describe('Server Tests', function(){
 
-  beforeEach(function(){
+  describe('User Login Tests', function(){
 
-    // Delete test users
+    // Delete test user
     User.findOneAndRemove({username: 'nicktest'}, function(err, obj) {
       if (err) {
         throw err;
       }
     });
-  });
 
-  it('Signup should save a new user to the database', function(done){
-    var options = {
-      'method': 'POST',
-      'uri': 'http://localhost:8080/users/signup',
-      'json': {
-        'username': 'nicktest',
-        'password': 'nickpass'
-      }
-    };
+    it('Signup should save a new user to the database', function(done){
+      var options = {
+        'method': 'POST',
+        'uri': 'http://localhost:8080/users/signup',
+        'json': {
+          'username': 'nicktest',
+          'password': 'nickpass'
+        }
+      };
 
-    request(options, function(error, res, body){
-      if (error) {
-        throw error;
-      } else {
-        User.findOne({username: 'nicktest'}, function(err, obj){
-          if (err) {
-            throw err;
-          } else if (body && body.token) {
-            var user = jwt.decode(body.token, 'secret').username;
-            expect(user).to.equal('nicktest');
-            done();
-          }
-        });
-      }
+      request(options, function(error, res, body){
+        if (error) {
+          throw error;
+          done();
+        } else {
+          User.findOne({username: 'nicktest'}, function(err, obj){
+            if (err) {
+              throw err;
+            } else if (body && body.token) {
+              var user = jwt.decode(body.token, 'secret').username;
+              expect(user).to.equal('nicktest');
+              done();
+            }
+          });
+        }
+      });
+
+    });
+
+    it('Signin should sign in an existing user', function(done){
+      var options = {
+        'method': 'POST',
+        'uri': 'http://localhost:8080/users/signin',
+        'json': {
+          'username': 'nicktest',
+          'password': 'nickpass'
+        }
+      };
+
+      request(options, function(error, res, body) {
+        if (error) {
+          throw error;
+          done();
+        } else {
+          User.findOne({username: 'nicktest'}, function(err, obj){
+            if (err) {
+              throw err;
+            } else {
+              var user = jwt.decode(body.token, 'secret').username;
+              expect(user).to.equal('nicktest');
+              done();
+            }
+          });
+        }
+      });
     });
 
   });
 
-  xit('should sign in an existing user', function(done){
-    
-    done();
-  });
-
-  xit('should logout a user that is signed in', function(done){
-    
-    done();
+  describe('EKG Data', function(){    
+    it('should receive a json object', function(done){
+      expect(3).to.equal(3);
+      done();
+    });
   });
 });
