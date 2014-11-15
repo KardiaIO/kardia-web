03c32d343b8a01448637332541ef3ffe4e4521d3
Nick Litwin <nicklitwin88@gmail.com>
2014-11-14 16:50:57 -0800

    Update comments

--- server/users/user-controller.js
+++ server/users/user-controller.js
@@ -9,7 +9,7 @@ var User = require('./user-model.js'), // user-schema
 
 module.exports = {
 
-  // sign-in function
+  // Sign-in function
   signin: function (req, res, next) {
     var username = req.body.username,
         password = req.body.password;
@@ -18,14 +18,14 @@ module.exports = {
     findUser({username: username})
       .then(function (user) {
         if (!user) {
-          // generates an error if user is not found in the system
+          // Generates an error if user is not found in the system
           next(new Error('User does not exist'));
         } else {
           return user.comparePasswords(password)
             .then(function(foundUser) {
               if (foundUser) {
                 var token = jwt.encode(user, 'secret');
-                // responds with a token when the user is found in the system
+                // Responds with a token when the user is found in the system
                 res.json({token: token});
               } else {
                 return next(new Error('No user'));
@@ -38,7 +38,7 @@ module.exports = {
       });
   }, //end signin
 
-  // sign-up function
+  // Sign-up function
   signup: function (req, res, next) {
     var username  = req.body.username,
         password  = req.body.password,
@@ -47,14 +47,14 @@ module.exports = {
 
     var findOne = Q.nbind(User.findOne, User);
 
-    // check to see if user already exists
+    // Check to see if user already exists
     findOne({username: username})
       .then(function(user) {
         if (user) {
-          // errors when when the user does already exist
+          // Errors when when the user does already exist
           next(new Error('User already exist!'));
         } else {
-          // make a new user if one doesn't exist
+          // Make a new user if one doesn't exist
           create = Q.nbind(User.create, User);
           newUser = {
             username: username,
@@ -64,7 +64,7 @@ module.exports = {
         }
       })
       .then(function (user) {
-        // create token to send back for auth using jwt
+        // Create token to send back for auth using jwt
         var token = jwt.encode(user, 'secret');
         res.json({token: token});
       })
@@ -73,8 +73,8 @@ module.exports = {
       });
   }, //end signup
 
-  // checking to see if the user is authenticated
-  // grab the token in the header is any
+  // Checking to see if the user is authenticated
+  // Grab the token in the header is any
   // then decode the token, which we end up being the user object
   // check to see if that user exists in the database
   checkAuth: function (req, res, next) {
@@ -98,7 +98,7 @@ module.exports = {
     }
   }, //end checkAuth
 
-  // this is the middleware function that will decode
+  // This is the middleware function that will decode
   // the username that identifies the user from which
   // the incoming request is coming
   decode: function (req, res, next) {
@@ -106,11 +106,11 @@ module.exports = {
     var token = req.headers['x-access-token'];
     var user;
     if (!token) {
-      // send forbidden if a token is not provided
+      // Send forbidden if a token is not provided
       return res.sendStatus(403); 
     }
     try {
-      // decode token and attach user to the request
+      // Decode token and attach user to the request
       // for use inside our controllers
       user = jwt.decode(token, 'secret');
       req.username = user.username;
