63c9217d9e3699a4647e3b7e2664883c70d8c725
radicalsauce <krhale@gmail.com>
2014-11-15 17:30:04 -0800

    Added email notification if problematic rhythm is detected

---
+++ server/problematic/rhythmNotification.js
@@ -0,0 +1,34 @@
+var nodemailer = require('nodemailer');
+
+module.exports = {
+
+  arrhythmiaNotify: function(user, email, timestamp){
+    // create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'ekgwebapi@gmail.com',
+            pass: 'ekgteam123!'
+        }
+    });
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'EKG Web App ✔ <ekgwebapi@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: 'ALERT: Arrhythmia detected', // Subject line
+        text: 'Hello '+ user +', ur heart is about to EXPLOOOOOODE <3', // plaintext body
+        html: '<b>Hello '+ user +', ur heart is about to EXPLOOOOOODE <3</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+          console.log(error);
+        } else {
+          console.log('Message sent: ' + info.response);
+        }
+    });
+  }
+
+}
--- server/python/pythonComm.js
+++ server/python/pythonComm.js
@@ -4,20 +4,21 @@ var client = new zerorpc.Client();
 client.connect("tcp://ekg-python12.cloudapp.net:4242");
 
 // checks whether connection to Python is present
-// client.invoke("hello", "Node!", function(error, res, more) {
-//     if(error){
-//       throw error;
-//     }
-//     console.log("Response from Python:", res);
-// });
+client.invoke("hello", "Node!", function(error, res, more) {
+    if(error){
+      throw error;
+    }
+    console.log("Response from Python:", res);
+});
 
 // Sends some data to Python, Python squares it - this is simply part
 // of testing the python connection and can be removed later
+
 client.invoke("processData", [1,2,3], function(error, res, more) {
   if(error){
     throw error;
   } 
   console.log(res);
-}); 
+});
 
 module.exports = client;
