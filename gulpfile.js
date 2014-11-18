ba26c38111a853d110bc1967ddf8b75f157172ee
Ryan Stellar <rstellar@gmail.com>
2014-11-18 14:55:19 -0800

    Closes #109 - Ability to start server by running 'gulp' at CLI

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var jshint = require('gulp-jshint');
 var mochaTest = require('gulp-mocha');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var server = require('gulp-express');
+
+gulp.task('server', function () {
+    // Start the server at the beginning of the task
+    require('./server.js');
+    //TODO: Restart the server when file changes - add a line for all files.
+    //gulp.watch(['server/**/*.html'], server.notify);
+});
 
 gulp.task('lint', function(){
   return gulp.src([
@@ -16,6 +24,7 @@ gulp.task('lint', function(){
       .pipe(jshint.reporter('jshint-stylish'))
       .pipe(jshint.reporter('fail'));
 });
+
 
 gulp.task('mochaTest', function(){
   return gulp.src(['tests/clientSpecs/clientSpecs.js', 'tests/serverSpecs/serverSpecs.js'])
@@ -41,7 +50,8 @@ gulp.task('watch', function(){
 // Run 'gulp' to lint and test your code
 gulp.task('default', [
   'lint',
-  'mochaTest'
+  'mochaTest',
+  'server'
 ]);
 
 // Run 'gulp build' to check your code and create a new concatenated file
@@ -57,3 +67,5 @@ gulp.task('travis', [
   'lint', 
   'mochaTest'
 ]);
+
+
