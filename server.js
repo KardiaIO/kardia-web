3e2c51d1905ecaa8be3e620db70c58fb1c1edbfc
Chao Xue <xuechao@alum.mit.edu>
2014-11-18 14:30:01 -0800

    Fix merge conflict

--- server.js
+++ server.js
@@ -41,6 +41,19 @@ var app = express();
 //     console.log("Response from Python:", res);
 // });
 
+var python = require('./python/pythonComm.js');
+
+// Sends some data to Python, Python squares it - this is simply part
+// of testing the python connection and can be removed later
+// python.invoke("processData", [1,2,3], function(error, res, more) {
+//   if(error){
+//     throw error;
+//   } 
+//   console.log(res);
+// }); 
+
+*/
+
 app.use(express.static(__dirname + '/client'));
 
 // var python = require('./python/pythonComm.js');
