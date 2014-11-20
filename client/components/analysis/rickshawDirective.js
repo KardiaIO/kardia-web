3d89d40215eab16180a7becf0015c69c48479589
Nick Litwin <nicklitwin88@gmail.com>
2014-11-19 16:01:49 -0800

    Refactor TimeFactory to have getter/setter

--- client/components/analysis/rickshawDirective.js
+++ client/components/analysis/rickshawDirective.js
@@ -9,7 +9,6 @@ angular.module('ekg.home')
     restrict: 'E',
     link: function postLink(scope, element, attrs) {
       scope.$watchCollection('[data, renderer]', function(newVal, oldVal){
-        console.log('watcher running');
         if(!newVal[0]){
           return;
         }
