73edaf9dc97fc36b8d16f3b25bcfea09029f3016
radicalsauce <krhale@gmail.com>
2014-11-21 10:03:40 -0800

    Changed mongo service

--- server/users/user-controller.js
+++ server/users/user-controller.js
@@ -1,6 +1,6 @@
 // Starting mongoDB connection
 var mongoose = require('mongoose');
-var mongoUrl = "mongodb://webEKGAPI:yhXk8EPXDSfy@ds051160.mongolab.com:51160/ekgapi";
+var mongoUrl = "mongodb://webEKGAPI:yhXk8EPXDSfy@ds051160.mongolab.com:53140/ekgmongo";
 mongoose.connect(mongoUrl);
 
 var User = require('./user-model.js'), // user-schema
