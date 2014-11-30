fff7ec19bebe33434ea707057207ed6576d23aac
Nick Litwin <nicklitwin88@gmail.com>
2014-11-30 15:53:36 -0800

    Add new clock

--- client/components/analysis/timeShift.js
+++ client/components/analysis/timeShift.js
@@ -36,9 +36,61 @@
       second: 'numeric'
     };
 
+
+      /* NEW CLOCK CODE */
+      Number.prototype.map = function ( in_min , in_max , out_min , out_max ) {
+      return ( this - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+      }
+
+      function rotate(element, degrees) {
+        element.css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                     '-moz-transform' : 'rotate('+ degrees +'deg)',
+                     '-ms-transform' : 'rotate('+ degrees +'deg)',
+                     'transform' : 'rotate('+ degrees +'deg)'});
+      }
+
+      function setClockTime(UTC) {
+        var date = new Date(UTC);
+        var sec = date.getSeconds();
+        var min = date.getMinutes();
+        var hrs = date.getHours();
+
+        var mapSec = sec.map(0, 59, 0, 360);
+        var mapMin = min.map(0,59,0,360);
+        var mapHrs = hrs.map(0,12,0,360);
+
+        if (sec == 0) {
+          $('.seconds-hand').removeClass('smooth');
+        } else {
+          $('.seconds-hand').addClass('smooth');
+        }
+
+        if (min == 0) {
+          $('.minute-hand').removeClass('smooth');
+        } else {
+          $('.minute-hand').addClass('smooth');
+        }
+
+        if (hrs == 0) {
+          $('.hour-hand').removeClass('smooth');
+        } else {
+          $('.hour-hand').addClass('smooth');
+        }
+
+        rotate($('.seconds-hand'), mapSec);
+        rotate($('.minute-hand'), mapMin);
+        rotate($('.hour-hand'), mapHrs);
+      }
+
+      /* END NEW CLOCK CODE */
+
+
     return {
       // The setTime function will reset the clock to a new time as well as change the display.
       setTime: function(UTC, context) {
+        /* NEW CLOCK CODE */
+        setClockTime(UTC);
+        /* END NEW CLOCK CODE */
         dateObject = new Date(UTC);
         clockTime = {
           date: dateObject.getDate(),
