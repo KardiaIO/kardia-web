e4be93accc7f4fb64c08489124a9871f17c046f5
Bernard Chu <bernard.chu@yahoo.com>
2014-12-20 14:38:21 -0800

    Add custom directive for password comparison error tooltip in signup.

--- client/components/developer/api/api.js
+++ client/components/developer/api/api.js
@@ -72,4 +72,22 @@ angular.module('ekg.api', [])
   	$state.transitionTo('documents');
   };
 
+})
+
+.directive("compareTo", function() {
+  return {
+    require: "ngModel",
+    scope: {
+      otherModelValue: "=compareTo"
+    },
+    link: function(scope, element, attributes, ngModel) {
+      ngModel.$validators.compareTo = function(modelValue) {
+        return modelValue === scope.otherModelValue;
+      };
+
+      scope.$watch("otherModelValue", function() {
+        ngModel.$validate();
+      });
+    }
+  };
 });
