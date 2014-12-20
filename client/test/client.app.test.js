3a17eccd7c1512241e25aac3531d6757db2f9643
Cory Asato <czasato@gmail.com>
2014-12-19 23:50:32 -0800

    app.js tests complete

--- client/test/client.app.test.js
+++ client/test/client.app.test.js
@@ -1,23 +1,42 @@
 describe('AttachTokensFactory', function() {
-  // Mocking Local Storage **FOR FUTURE USE YOU BRAVE SOUL**
-  var mockStorage = {
-    setItem: function() {},
-    removeItem: function() {},
-    key: function() {},
-    getItem: function(string) {
-      return this[string];
-    },
-    length: 0
-  };
   var AttachTokensFactory;
+  var sandbox;
+  var store = {};
 
+  // Get Factory Module
   beforeEach(module('ekg'));
+  // Injecting Factory into local variable & setting up LocalStorage stubs.
   beforeEach(inject(function(_AttachTokens_) {
     AttachTokensFactory = _AttachTokens_;
+    sandbox = sinon.sandbox.create();
+
+    sandbox.stub(window.localStorage, 'setItem', function(key, value) {
+      store[key] = value;
+    });
+    sandbox.stub(window.localStorage, 'getItem', function(key) {
+      return store[key];
+    });
   }));
+
+  afterEach(function() {
+    sandbox.restore();
+    store = {};
+  });
 
   it('should return an object with a request method', function() {
     expect(AttachTokensFactory).to.include.keys('request');
   });
 
+  it('should get key/jwt tokens from localStorage', function() {
+    // Creates a fake header object to append fake jwt token to.
+    var headerWrapper = {
+      headers: {}
+    };
+    // Set token in localStorage so that our factory can retrieve it.
+    window.localStorage.setItem('com.ekgtracker', 'some-long-string');
+    // Returns the 'attach' object with headers appended.
+    var response = AttachTokensFactory.request(headerWrapper);
+    expect(response.headers['x-access-token']).to.equal('some-long-string');
+    expect(response.headers['Allow-Control-Allow-Origin']).to.equal('*');
+  });
 });
