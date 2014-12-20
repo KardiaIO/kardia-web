e75f7ec0ec6223de1f8732f2fcd2cdc372d935a2
Bernard Chu <bernard.chu@yahoo.com>
2014-12-20 14:44:13 -0800

    Copy new forms to user side.

--- client/components/signin/auth.js
+++ client/components/signin/auth.js
@@ -1,7 +1,20 @@
 angular.module('ekg.auth', [])
 
 .controller('AuthController', function ($scope, $window, $state, Auth, $location) {
+  $scope.isSigningIn = true;
 
+  $scope.signingIn = function() {
+    return !$scope.isAuth() && $scope.isSigningIn;
+  };
+
+  $scope.signingUp = function() {
+    return !$scope.isAuth() && !$scope.isSigningIn;
+  };
+
+  $scope.switchView = function() {
+    $scope.isSigningIn = !$scope.isSigningIn;
+  };
+  
   $scope.user = {};
 
   $scope.signin = function () {
@@ -22,43 +35,17 @@ angular.module('ekg.auth', [])
   };
 
   $scope.signup = function () {
-    var isValid = false;
-    // The sign up function will perform the following checks in order: 
-    // 1. Are the two passwords the same
-    // 2. Is the email entered a valid email address
-    // 3. Is the password more than 6 characteristics
-    // 4. Did the user put something in the first and last name fields
-    if ($scope.user.password === $scope.user.cpassword) {
-      if ($scope.user.username.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        if ($scope.user.password.length >= 6) {
-          if ($scope.user.first.length > 0 && $scope.user.last.length > 0){
-            isValid = true;
-          } else {
-            alert('Please enter your first and last name.');
-          }
+    Auth.signup($scope.user)
+      .then(function (token) {
+        if (token) {
+          $window.localStorage.setItem('com.ekgtracker', token);
         } else {
-          alert('Your password needs to be at least 6 characters long.');
+          alert('Your email is already registered.');
         }
-      } else {
-        alert('Your email address is invalid.');
-      }
-    } else {
-      alert('Your passwords do not match.');
-    }
-    if (isValid){
-      Auth.signup($scope.user)
-        .then(function (token) {
-          if (token) {
-            $window.localStorage.setItem('com.ekgtracker', token);
-            $state.transitionTo('user.triage');
-          } else {
-            alert('Your email is already registered.');
-          }
-        })
-        .catch(function (error) {
-          alert('Error in signup function: ', error);
-        });
-    }
+      })
+      .catch(function (error) {
+        alert('Error in signup function: ', error);
+      });
   };
 
   $scope.isAuth = Auth.isAuth;
