4e0243d5b968daf3d6658deecfe3ca1d6079867f
Bernard Chu <bernard.chu@yahoo.com>
2014-12-20 14:10:54 -0800

    Remove unnecessary checks in signin/up functions.

--- client/components/developer/api/api.js
+++ client/components/developer/api/api.js
@@ -52,42 +52,17 @@ angular.module('ekg.api', [])
   };
 
   $scope.signup = function () {
-    var isValid = false;
-    // The sign up function will perform the following checks in order: 
-    // 1. Are the two passwords the same
-    // 2. Is the email entered a valid email address
-    // 3. Is the password more than 6 characteristics
-    // 4. Did the user put something in the first and last name fields
-    if ($scope.user.password === $scope.user.cpassword) {
-      if ($scope.user.username.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        if ($scope.user.password.length >= 6) {
-          if ($scope.user.first.length > 0 && $scope.user.last.length > 0){
-            isValid = true;
-          } else {
-            alert('Please enter your first and last name.');
-          }
+    Auth.signup($scope.user)
+      .then(function (token) {
+        if (token) {
+          $window.localStorage.setItem('com.ekgtracker', token);
         } else {
-          alert('Your password needs to be at least 6 characters long.');
+          alert('Your email is already registered.');
         }
-      } else {
-        alert('Your email address is invalid.');
-      }
-    } else {
-      alert('Your passwords do not match.');
-    }
-    if (isValid){
-      Auth.signup($scope.user)
-        .then(function (token) {
-          if (token) {
-            $window.localStorage.setItem('com.ekgtracker', token);
-          } else {
-            alert('Your email is already registered.');
-          }
-        })
-        .catch(function (error) {
-          alert('Error in signup function: ', error);
-        });
-    }
+      })
+      .catch(function (error) {
+        alert('Error in signup function: ', error);
+      });
   };
 
   $scope.isAuth = Auth.isAuth;
