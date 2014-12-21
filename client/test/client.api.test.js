48d77494d157b226e88a198293ba5e0999cbaf48
cdepman <cdepman@gmail.com>
2014-12-20 16:59:59 -0800

    continues building out api client test

--- client/test/client.api.test.js
+++ client/test/client.api.test.js
@@ -1,36 +1,27 @@
 describe('APIController', function () {
-  var $httpBackend, $rootScope, createController, generateKey, APIController, $state;
+  var $httpBackend, $rootScope, $controller, $state, createController, generateKey, APIController;
   // set up the module 
+  beforeEach(module('ui.router'));
+  beforeEach(module('ekg.auth'));
   beforeEach(module('ekg.api'));
 
-  beforeEach(inject(function($injector){
-    console.log('EHLLLOW????!@??!@!@?');
-    $httpBackend = $injector.get('$httpBackend');
+  beforeEach(inject(function($injector, _$httpBackend_, _$rootScope_, _$state_, _Auth_, _$window_, _$controller_){
+    $httpBackend = _$httpBackend_;
     generateKey = $httpBackend.when('GET', '/api/keys')
-                            .respond({id: 'API Key'},{secret: 'SecureID'});
+                            .respond({data: {id: 'API Key', secret: 'SecureID'} });
     // get hold of a scope and state
-    var $rootScope = $injector.get('$rootScope');
-    var $state = $injector.get('$state');
-    var $window = $injector.get('$window');
-    var Auth = $injector.get('Auth');
+    $rootScope = _$rootScope_;
+    $state = _$state_;
+    Auth = _Auth_;
+    $window = _$window_;
     // the controller service is used to create instances of controllers
-    var $controller = $injector.get('$controller');
-
-    console.log('ALL STUFF: ', {
-        '$scope': $rootScope, 
-        '$window': $window,
-        '$state': $state,
-        '$http': $httpBackend, 
-        'Auth': Auth
-      })
-
+    $controller = _$controller_;
 
     createController = function () {
       return $controller('APIController', {
         '$scope': $rootScope, 
         '$window': $window,
         '$state': $state,
-        '$http': $httpBackend, 
         'Auth': Auth
       });
     };
@@ -43,11 +34,11 @@ describe('APIController', function () {
 
   it('Should generate API key', function () {
     var controller = createController();
-    $httpBackend.flush();
     $httpBackend.expectGET('/api/keys');
-    expect($rootScope.APIkey).toBe('API Key');
-    expect($rootScope.SecureID).toBe('SecureID');
-    $httpBackend.flush();
+    $rootScope.$digest();
+    expect($rootScope.generated).to.be.true;
+    // expect($rootScope.APIkey).to.equal('API Key');
+    // expect($rootScope.SecureID).to.equal('SecureID');
   });
 
 });
