f5fcf969ae1a43bdd49f3354c9388e74d9adcbd3
Ryan <1404373+rstellar@users.noreply.github.com>
2018-09-26 23:30:34 -0700

    ryan testing github integration on september 26th

--- client/scratch.js
+++ client/scratch.js
@@ -1,3 +1,4 @@
 //ryan is adding this line to test 1461
 //dummy pull request on Aug 6
 //ryan testing at 532 on Friday sept 14th. 
+//creating a pull requestd to test github integraiton on september 26
